1119009a8c332ea4c3b2d6a3a2ccc710740ad317
atsuko <atsuko@ibm.com>
2021-06-11 16:06:44 -0400

    Add test_c_scale

--- Quantum-Music/tests/test_scales.py
+++ Quantum-Music/tests/test_scales.py
@@ -2,11 +2,10 @@ from quantum_music.scales import get_scale, c_scale
 
 
 class TestScales:
-    def test_get_scale(self):
+    def test_c_scale(self):
         start_note = ("C4", 261.63 * 2)
-        scale = get_scale(start_note)
+        scale = get_scale(start_note, pi_division=4)
 
-        for phase in scale.keys():
-            print(f"phase: {phase}")
-            print("scale", scale[phase])
-            print("c_scale", c_scale[phase])
+        for phase in c_scale:
+            freq_diff = scale[phase][1] - c_scale[phase][1]
+            assert freq_diff < 0.1
